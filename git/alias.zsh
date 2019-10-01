6c933f037cb3dd258cea994211ec93f4db55c3d8
Andreas Sander <mail@andi1984.de>
2019-10-01 09:01:01 +0200

    Adding shell aliases for git.

---
+++ git/alias.zsh
@@ -0,0 +1,2 @@
+alias gbpurge="git branch --merged | grep -v \"*\" | grep -Ev \"(\*|master|develop|staging)\" | xargs -n 1 git branch -d"
+alias gbclean="git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -d"
