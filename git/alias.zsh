56d57a27eb03a883876e8d4288f87d658b3969ee
Andreas Sander <mail@andi1984.de>
2019-10-10 10:16:53 +0200

    Add Git Alias - GToday for branches checked out in last 24h

--- git/alias.zsh
+++ git/alias.zsh
@@ -1,3 +1,4 @@
 alias gbpurge="git branch --merged | grep -v \"*\" | grep -Ev \"(\*|master|develop|staging)\" | xargs -n 1 git branch -d"
 alias gbclean="git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -d"
 alias gpo='git push origin "$(git symbolic-ref --short HEAD)"'
+alias gtoday='git lastbranches| grep "Stunden"'
