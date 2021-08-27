10e6a8af4366cc55b6537cb1eaafa4bb016b6362
Andreas Sander <andi@uberresearch.com>
2021-08-27 14:40:33 +0200

    Add fzy-native

--- vim/init.lua
+++ vim/init.lua
@@ -41,6 +41,7 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-telescope/telescope-fzy-native.nvim'
 Plug 'dense-analysis/ale'
 Plug 'gruvbox-community/gruvbox'
 Plug 'neovim/nvim-lsp'
