0c7f0e0aea6b48e7b44b49924c01d73cf299a60d
Andreas Sander <mail@andi1984.de>
2021-09-07 23:52:32 +0200

    adding a few plugins

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -10,5 +10,20 @@ Plug 'neovim/nvim-lsp'
 Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install()']})
 Plug 'junegunn/fzf.vim'
 Plug('styled-components/vim-styled-components', { ['branch'] = 'main' })
+
+--Install nvim-cmp
+Plug 'hrsh7th/nvim-cmp'
+
+-- Install snippet engine (This example installs [hrsh7th/vim-vsnip](https://github.com/hrsh7th/vim-vsnip))
+Plug 'hrsh7th/vim-vsnip'
+
+-- Install the buffer completion source
+Plug 'hrsh7th/cmp-buffer'
+
+Plug 'hoob3rt/lualine.nvim'
+Plug 'kyazdani42/nvim-web-devicons'
+Plug 'folke/trouble.nvim'
+Plug ('nvim-treesitter/nvim-treesitter', {['do'] = vim.fn[':TSUpdate']})  -- We recommend updating the parsers on update
+
 vim.call('plug#end')
 
---
+++ vim/plugin/tree.vim
@@ -0,0 +1,5 @@
+let g:nvim_tree_auto_open = 1 "0 by default, opens the tree when typing `vim $DIR` or `vim`
+nnoremap <C-n> :NvimTreeToggle<CR>
+nnoremap <leader>tr :NvimTreeRefresh<CR>
+nnoremap <leader>tf :NvimTreeFindFile<CR>
+nnoremap <leader>to :NvimTreeOpen<CR>
