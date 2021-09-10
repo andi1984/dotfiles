ed70fbb5d9e30bbc2454e3dd0f1fbd7d86f856f6
Andreas Sander <andi@uberresearch.com>
2021-09-10 12:57:46 +0200

    Update keymap

--- vim/lua/keymap.lua
+++ vim/lua/keymap.lua
@@ -1,7 +1,11 @@
+-- Finding files
 vim.api.nvim_set_keymap('n', '<Leader>ff',  [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>fb',  [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>fi', ':Rg ', { noremap = true })
+--vim.api.nvim_set_keymap('n', '<Leader>fif', ':Rg <EXPR> -g "*.{js,jsx,ts,tsx}"', { noremap = true, 'expr': v.true })
+
+-- Moving lines up/down
 vim.api.nvim_set_keymap('n', '<S-Down>',  ':m+<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-Up>',  ':m-2<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<S-Down>',  ':m\'>+1<CR>gv=gv', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<S-Up>',  ':m\'<-2<CR>gv=gv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>fi', ':Rg<CR>', { noremap = true })
