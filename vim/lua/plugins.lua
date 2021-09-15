abb5659be8fafd78333a38e4642dddd896f63d2f
Andreas Sander <andi@uberresearch.com>
2021-09-15 09:03:56 +0200

    Undo all completion related plugins

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -11,15 +11,6 @@ Plug('junegunn/fzf', {['do'] = vim.fn['fzf#install()']})
 Plug 'junegunn/fzf.vim'
 Plug('styled-components/vim-styled-components', { ['branch'] = 'main' })
 
---Install nvim-cmp
-Plug 'hrsh7th/nvim-cmp'
-
--- Install snippet engine (This example installs [hrsh7th/vim-vsnip](https://github.com/hrsh7th/vim-vsnip))
-Plug 'hrsh7th/vim-vsnip'
-
--- Install the buffer completion source
-Plug 'hrsh7th/cmp-buffer'
-
 Plug 'hoob3rt/lualine.nvim'
 Plug 'kyazdani42/nvim-web-devicons'
 Plug 'folke/trouble.nvim'
