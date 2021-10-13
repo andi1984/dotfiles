472c90b793c3d0eeb187e747dd805f584a2f2081
Andreas Sander <mail@andi1984.de>
2021-10-13 23:18:47 +0200

    Change telescope config showing hidden files

---
+++ vim/plugin/telescope.lua
@@ -0,0 +1,13 @@
+ require("telescope").setup{
+    defaults = {
+      path_display = {
+        "shorten",
+        "absolute"
+      },
+    },
+    pickers = {
+      find_files = {
+        hidden = true
+      }
+    }
+}
