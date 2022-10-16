e30dee57462b4e8d7aab14db4e955476289c33f4
Andreas Sander <mail@andi1984.de>
2022-10-16 12:46:30 +0200

    wip

--- vim/plugin/lsp.lua
+++ vim/plugin/lsp.lua
@@ -38,7 +38,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'pylsp', 'vuels', 'rust_analyzer'}
+local servers = { 'pylsp', 'vuels', 'rust_analyzer', 'tailwindcss'}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
