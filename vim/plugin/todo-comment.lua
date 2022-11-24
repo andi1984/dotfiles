6efcbc0fd2ad191c6cb0efe99afc6abe3213018b
Andreas Sander <mail@andi1984.de>
2022-11-24 23:14:06 +0100

    Remove todo-comment as it returns lot of erros right now

--- vim/plugin/todo-comment.lua
+++ vim/plugin/todo-comment.lua
@@ -1,4 +1,5 @@
- require("todo-comments").setup {
+--[[
+require("todo-comments").setup {
   signs = true, -- show icons in the signs column
   sign_priority = 8, -- sign priority
   -- keywords recognized as todo comments
@@ -29,7 +30,9 @@
     before = "", -- "fg" or "bg" or empty
     keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
     after = "fg", -- "fg" or "bg" or empty
-    pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+--]]
+    -- pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+    --[[
     comments_only = true, -- uses treesitter to match keywords in comments only
     max_line_len = 400, -- ignore lines longer than this
     exclude = {}, -- list of file types to exclude highlighting
@@ -55,7 +58,9 @@
     },
     -- regex that will be used to match keywords.
     -- don't replace the (KEYWORDS) placeholder
-    pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-    -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-  },
+    --]]
+    -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex    -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+--[[
+},
 }
+--]]
