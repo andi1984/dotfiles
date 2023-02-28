86e326a847a1fe1f21a47396510db37a913e2fbe
Andreas Sander <mail@andi1984.de>
2023-02-28 22:28:48 +0100

    update neodim

--- vim/plugin/neodim.lua
+++ vim/plugin/neodim.lua
@@ -1,7 +1,3 @@
 require("neodim").setup({
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
-  alpha = 0.5,
-  blend_color = "#10171f"
+        alpha = 0.2,
 })
