e3a62f7cafba039e507bd17aac65febf551e3c13
Andreas Sander <andi@uberresearch.com>
2023-09-13 09:37:55 +0200

    update macos zsh config

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,4 +1,18 @@
-source ../git/alias.zsh
+source $(dirname $0)/../git/alias.zsh
 
 alias cdWork='cd ~/dev/web'
 alias nfix='nvm use'
+
+# Integration deploy
+alias use_integration="export KUBECONFIG=$HOME/Documents/integration2.yaml"
+alias use_development="export KUBECONFIG=$HOME/Documents/development.yaml"
+# Kube aliases
+alias k="kubectl --insecure-skip-tls-verify"
+alias kns="kubectl --insecure-skip-tls-verify get namespaces"
+alias kc="kubectl --insecure-skip-tls-verify config set-context --current --namespace "
+alias kcc="kubectl --insecure-skip-tls-verify config get-contexts"
+alias kd="kubectl --insecure-skip-tls-verify describe "
+alias ka="kubectl --insecure-skip-tls-verify get all"
+alias kl="kubectl --insecure-skip-tls-verify logs "
+alias kin="kubectl --insecure-skip-tls-verify get ingress"
+alias kn="kubectl --insecure-skip-tls-verify get nodes --show-labels"
--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,5 +1,5 @@
 # Path for homebrew (/usr/local/[s]bin)
-export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 export PATH="$HOME/.yarn/bin:$PATH"
 export PATH="/usr/local/opt/postgresql@9.5/bin:$PATH"
 export PATH="/usr/local/opt/php@7.1/bin:$PATH"
@@ -7,3 +7,5 @@ export PATH="/usr/local/opt/php@7.1/sbin:$PATH"
 export PATH="$HOME/.deno:$PATH"
 export PATH="$(pyenv root)/shims:$PATH"
 export PATH="$HOME/dev/dotfiles/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.cargo/env:$PATH"
