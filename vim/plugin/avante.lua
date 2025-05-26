19557058404d041b9731e0439e8a0b9cc8a18204
Andreas Sander <andi@uberresearch.com>
2025-05-26 23:29:51 +0200

    bump plugins

--- vim/plugin/avante.lua
+++ vim/plugin/avante.lua
@@ -1,4 +1,4 @@
 require('avante').setup {
         ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-  -- provider = "", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+  provider = "copilot", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
 }
